308ab5947836eddd86693d4f1f90df445464c543
Mateusz Kuczynski <mkuczynski0074@gmail.com>
2019-03-26 22:24:18 +0100

    Added Person Class

--- src/com/BMI/Person.java
+++ src/com/BMI/Person.java
@@ -1,4 +1,26 @@
 package com.BMI;
 
-public class Person {
+public class Person
+{
+    private double weight;
+    private double height;
+    private int age;
+
+    public Person(double weight, double height, int age) {
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
